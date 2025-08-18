b1bc78a8b42632b964d4e75672ef7f5b89243b46
moulimds <moulidas106@gmail.com>
2025-08-18 15:44:03 +0530

    change

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,7 +5,7 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 import json
 
-BASE_URL = "http://localhost:8000"
+BASE_URL = "http://127.0.0.1:8000"
 
 @pytest.mark.asyncio
 async def test_load_performance():
